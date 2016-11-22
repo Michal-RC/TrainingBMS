a46c1efd5f89750c18472ef874621a4b655b2b9a
mgolkowski <mgolkowski@7bulls.com>
2016-11-22 15:04:14 +0100

    commit msg

--- src/eu/sig/training/ch03/BinaryTreeSearch.java
+++ src/eu/sig/training/ch03/BinaryTreeSearch.java
@@ -4,29 +4,30 @@ import eu.sig.training.ch03.TreeException;
 import eu.sig.training.ch03.BinaryTreeNode;
 
 public class BinaryTreeSearch {
-	
+
+    void compareNodeValues(param) {
+            if (param == null) {
+                throw new TreeException("Value not found in tree!");
+            } else {
+                return 1 + calculateDepth(param, nodeValue);
+            }
+    }
     // tag::calculateDepth[]
     public static int calculateDepth(BinaryTreeNode<Integer> node, int nodeValue) {
         if(node == null){
         	throw new IllegalArgumentException("A reference to argument 'node' was not specified");
         }
     	int depth = 0;
+        void compareValues()
         if (node.getValue() == nodeValue) {
             return depth;
         } else {
             if (nodeValue < node.getValue()) {
                 BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
+                compareNodeValues(left);
             } else {
                 BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
+                compareNodeValues(right);
                 }
             }
         }
--- src/eu/sig/training/ch04/SavingsAccount.java
+++ src/eu/sig/training/ch04/SavingsAccount.java
@@ -6,28 +6,46 @@ import eu.sig.training.ch04.Money;
 public class SavingsAccount {
     CheckingAccount registeredCounterAccount;
 
+    void checkingRegistered() {
+        if (result.getCounterAccount().equals(this.registeredCounterAccount))
+        {
+            return result;
+        } else {
+            throw new BusinessException("Counter-account not registered!");
+        }
+    }
+
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            sum = sum + (9 - i) * Character.getNumericValue(
-                counterAccount.charAt(i));
-        }
+//         1. Assuming result is 9-digit bank account number, validate 11-test:
+        int sum = getSum(counterAccount);
         if (sum % 11 == 0) {
             // 2. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount); // <2>
             // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
-                return result;
-            } else {
-                throw new BusinessException("Counter-account not registered!");
-            }
+            return getTransfer(result);
         } else {
             throw new BusinessException("Invalid account number!!");
         }
+    }
+
+    private Transfer getTransfer(Transfer result) throws BusinessException {
+        if (result.getCounterAccount().equals(this.registeredCounterAccount))
+        {
+            return result;
+        } else {
+            throw new BusinessException("Counter-account not registered!");
+        }
+    }
+
+    private int getSum(String counterAccount) {
+        int sum = 0; // <1>
+        for (int i = 0; i < counterAccount.length(); i++) {
+            sum = sum + (9 - i) * Character.getNumericValue(
+                counterAccount.charAt(i));
+        }
+        return sum;
     }
 }
 // end::SavingsAccount[]
